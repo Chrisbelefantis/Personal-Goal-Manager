b5719607e2b0a786d540ca19d90b7052a084ae5c
Chris Belefantis <chrisbelefadis@gmail.com>
2021-10-10 19:12:59 +0300

    Update axios-instance.js

--- react-app/src/axios-instance.js
+++ react-app/src/axios-instance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'ec2-18-130-225-215.eu-west-2.compute.amazonaws.com:3001/'
+    baseURL: 'http://ec2-18-130-225-215.eu-west-2.compute.amazonaws.com:3001/'
     // baseURL: 'http://localhost:3001/'
 });
 
